7ecd75bdafc3756323de96af1a1ffbd1c4e85b1a
ragavarshini3 <alagarsamyvarshini@gamil.com>
2025-03-21 11:45:29 +0530

    Update

--- main.c
+++ main.c
@@ -13,45 +13,58 @@
  * Now, write your code! You got this! 💪🔥
  */
 
-#include <stdio.h>
+ #include <stdio.h>
 
-#define MAX_ROOMS 5  //  max number of rooms
-
-// Function prototypes
-void initializeSystem();
-void displayMenu();
-void controlLights();
-void readTemperature();
-void detectMotion();
-void securitySystem();
-void analyzeHouseStatus();
-
-int main() {
-    
-
-    return 0;
-}
-
+ #define MAX_ROOMS 5  // Max number of rooms
  
-void initializeSystem() {
-  
-}
-
-void displayMenu() {
-   
-}
-void controlLights() {
-
-}
-void readTemperature() {
-
-}
-void detectMotion() {
-
-}
-void securitySystem() {
-
-}
-void analyzeHouseStatus() {
-
-}
+ // Function prototypes
+ void initializeSystem(int lights[], int temperatures[], int motion[], int locks[]);
+ void displayMenu();
+ void controlLights(int lights[]);
+ void readTemperature(int temperatures[]);
+ void detectMotion(int motion[]);
+ void securitySystem(int locks[]);
+ void analyzeHouseStatus(int lights[], int temperatures[], int motion[], int locks[]);
+ 
+ int main() {
+     int lights[MAX_ROOMS];       // Stores ON/OFF states
+     int temperatures[MAX_ROOMS]; // Stores temperature values
+     int motion[MAX_ROOMS];       // Stores motion detection (1 for detected, 0 for none)
+     int locks[MAX_ROOMS];        // Stores lock states (1 for locked, 0 for unlocked)
+     
+     initializeSystem(lights, temperatures, motion, locks); // Initialize system
+ 
+     int choice;
+     while (1) {
+         displayMenu();
+         printf("Enter your choice: ");
+         scanf("%d", &choice);
+ 
+         switch (choice) {
+             case 1:
+                 controlLights(lights);
+                 break;
+             case 2:
+                 readTemperature(temperatures);
+                 break;
+             case 3:
+                 detectMotion(motion);
+                 break;
+             case 4:
+                 securitySystem(locks);
+                 break;
+             case 5:
+                 analyzeHouseStatus(lights, temperatures, motion, locks);
+                 break;
+             case 6:
+                 printf("Exiting... Goodbye!\n");
+                 return 0;
+             default:
+                 printf("Invalid choice! Please select a valid option.\n");
+         }
+     }
+ 
+     return 0;
+ }
+ 
+ 
